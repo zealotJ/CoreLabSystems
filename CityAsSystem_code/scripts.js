93d3011fc0e773fd000c9561900f26f3ab69337a
zealotJ <60146904+zealotJ@users.noreply.github.com>
2021-11-08 16:34:50 -0500

    Add files via upload

---
+++ CityAsSystem_code/scripts.js
@@ -0,0 +1,47 @@
+function pop1(){
+    document.getElementById("popup1").style.visibility="visible"
+}
+
+function close1(){
+    document.getElementById("popup1").style.visibility="hidden"
+}
+
+function pop2(){
+    document.getElementById("popup2").style.visibility="visible"
+}
+
+function close2(){
+    document.getElementById("popup2").style.visibility="hidden"
+}
+
+function pop3(){
+    document.getElementById("popup3").style.visibility="visible"
+}
+
+function close3(){
+    document.getElementById("popup3").style.visibility="hidden"
+}
+
+function pop4(){
+    document.getElementById("popup4").style.visibility="visible"
+}
+
+function close4(){
+    document.getElementById("popup4").style.visibility="hidden"
+}
+
+function pop5(){
+    document.getElementById("popup5").style.visibility="visible"
+}
+
+function close5(){
+    document.getElementById("popup5").style.visibility="hidden"
+}
+
+function pop6(){
+    document.getElementById("popup6").style.visibility="visible"
+}
+
+function close6(){
+    document.getElementById("popup6").style.visibility="hidden"
+}
